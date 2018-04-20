8dc46ae0861b4eb65bddbe049080b1175605e4d5
Jon Blackburn <jonblackburn@jons-macbook-pro.local>
2018-04-19 21:58:53 -0400

    Initial Commit

---
+++ GratitudeJournal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GratitudeJournal
+//
+//  Created by Jon Blackburn on 4/19/18.
+//  Copyright © 2018 Jon Blackburn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
